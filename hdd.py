2436a3d00304c5ce6271cfc5c0f042f48a05427e
nobaene <50325778+nobaene@users.noreply.github.com>
2019-12-04 13:16:58 +0200

    imported subprocess and Popen

--- hdd.py
+++ hdd.py
@@ -1,2 +1,5 @@
 # This is a script to read the server hard disk space available on the file systems and the hostnames.
 # Centos-Server, Raspbery-Pi1, Mac-Book and LPIC
+
+import subprocess
+from subprocess import Popen
